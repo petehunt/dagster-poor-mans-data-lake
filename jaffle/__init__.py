22050e247d485c3d79f08138c0a47ba3cb2d2398
Nicholas Schrock <schrockn@elementl.com>
2022-12-16 09:15:32 -0500

    unresource-ify to some degree

--- jaffle/__init__.py
+++ jaffle/__init__.py
@@ -1,26 +1,18 @@
-from dagster import Definitions, load_assets_from_package_module, io_manager
+from dagster import Definitions, load_assets_from_package_module
 
 from jaffle import assets
 
 from jaffle.duckpond import DuckPondIOManager, DuckDB
 
-
-duckdb_localstack = DuckDB("""
+DUCKDB_LOCAL_CONFIG="""
 set s3_access_key_id='test';
 set s3_secret_access_key='test';
 set s3_endpoint='localhost:4566';
 set s3_use_ssl='false';
 set s3_url_style='path';
 """
-)
-
-
-@io_manager(required_resource_keys={"duckdb"})
-def duckpond_io_manager(init_context):
-    return DuckPondIOManager("datalake", init_context.resources.duckdb)
-
 
 defs =  Definitions(
     assets=load_assets_from_package_module(assets),
-    resources={"io_manager": duckpond_io_manager, "duckdb": duckdb_localstack},
+    resources={"io_manager":  DuckPondIOManager("datalake", DuckDB(DUCKDB_LOCAL_CONFIG))}
 )
