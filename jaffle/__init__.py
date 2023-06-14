b2aa5a06ef7ee1701e41d3abe4a1b7e281f24c20
Pete Hunt <floydophone@gmail.com>
2023-06-14 05:39:46 +0000

    Migrate to motherduck

--- jaffle/__init__.py
+++ jaffle/__init__.py
@@ -2,9 +2,10 @@ from dagster import Definitions, load_assets_from_package_module
 
 from jaffle import assets
 
-from jaffle.duckpond import DuckPondIOManager, DuckDB
+from jaffle.duckpond import DuckPondIOManager, MotherduckIOManager, DuckDB
+import os
 
-DUCKDB_LOCAL_CONFIG="""
+DUCKDB_LOCAL_CONFIG = """
 set s3_access_key_id='test';
 set s3_secret_access_key='test';
 set s3_endpoint='localhost:4566';
@@ -14,5 +15,9 @@ set s3_url_style='path';
 
 defs = Definitions(
     assets=load_assets_from_package_module(assets),
-    resources={"io_manager":  DuckPondIOManager("datalake", DuckDB(DUCKDB_LOCAL_CONFIG))}
+    # Uncomment the following line to use local DuckDB + S3/parquet
+    # resources={"io_manager":  DuckPondIOManager("datalake", DuckDB(DUCKDB_LOCAL_CONFIG))}
+    resources={
+        "io_manager": MotherduckIOManager(DuckDB(url=os.environ["MOTHERDUCK_URL"]))
+    },
 )
